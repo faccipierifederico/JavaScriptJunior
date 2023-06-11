280f69aa45391424feba584a6ef83fa632e0b88c
Federico Faccipieri <federicofaccipieri@gmail.com>
2023-06-11 10:08:34 -0300

    Task 2 - Excercise 2

--- index_junior2.js
+++ index_junior2.js
@@ -36,3 +36,39 @@
 
 // EJERCICIO 2
 
+// Crear un sistema que permita registrar alumnos presentes (P) y ausentes (A) en una clase.
+// Pasados los 30 días de cursada, mostrar cuántos presentes tuvo y cuántos ausentes.
+// Se pueden tener un máximo de 10% de ausencias, sino, se reprueba.
+
+let cantidad = prompt("¿Cuántos alumnos son?");
+let alumnosTotales = [];
+
+for (i = 0; i < cantidad; i++) {
+    alumnosTotales[i] = [prompt("Nombre del alumno " + (i+1)), 0];
+}
+
+const tomarAsistencia = (nombre, p) => {
+    let presencia = prompt(nombre);
+    if (presencia == "p" || presencia == "P") {
+        alumnosTotales[p][1]++;
+    }
+}
+
+for (i = 0; i < 30; i++) {
+    for (alumno in alumnosTotales) {
+        tomarAsistencia(alumnosTotales[alumno][0], alumno)
+    }
+}
+
+for (alumno in alumnosTotales) {
+    let resultado = `${alumnosTotales[alumno][0]} <br>
+    Presentes: <b> ${alumnosTotales[alumno][1]} </b> <br>
+    Ausentes: <b> ${30 - alumnosTotales[alumno][1]} </b>`;
+
+    if (30 - alumnosTotales[alumno][1] > 3) {
+        resultado+= `<b> Reprobado por inasistencia </b> <br> <br>`;
+    } else {
+        resultado+= `<br><br>`;
+    }
+    document.write(resultado);
+}
